b0739d2dc0d71185d3e796038a5c4fc410e23e3d
dev-2031887 <102762254+Froggy1298@users.noreply.github.com>
2023-03-08 15:28:46 -0500

    sdaf,jk

---
+++ SolutionESP/CaisseEnregistreuseTest/PanierVueModelTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CaisseEnregistreuse.ViewModel;
+using FluentAssertions;
+
+namespace CaisseEnregistreuseTest
+{
+    [TestClass]
+    public class PanierVueModelTest
+    {
+
+        [TestMethod]
+        public void TestConstructor()
+        {
+            PanierVueModel PanierVm = new PanierVueModel();
+            PanierVm.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public void Alloa()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
---
+++ SolutionESP/GestionInventaireTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace GestionInventaireTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
